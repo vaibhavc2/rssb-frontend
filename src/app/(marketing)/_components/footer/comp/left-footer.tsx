9d7ba63c7b580619888d73ff910ecc219c3afb01
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-10-19 09:15:43 +0530

    start doing footer , start setup for other hooks

---
+++ src/app/(marketing)/_components/footer/comp/left-footer.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+
+const LeftFooter = () => {
+  return (
+    <div className="flex w-1/2 flex-col items-center justify-center gap-x-2 text-center text-muted-foreground hover:text-gray-600 md:ml-auto md:justify-end">
+      <Button
+        variant={"ghost"}
+        size={"sm"}
+        className="hover:bg-inherit hover:text-gray-600"
+      >
+        Developer:
+      </Button>
+      {/* Create separate components later: */}
+
+      {/* //TODO: show following: only on larger screens */}
+      {/* //TODO: Github profile photo with link */}
+      {/* //TODO: LinkedIn profile link */}
+      <Button variant={"ghost"} size={"sm"} className="hover:bg-inherit">
+        <a
+          href={"https://github.com/vaibhavc2"}
+          className="text-md transition-all hover:border-b-1 hover:border-b-gray-800 hover:pb-1 hover:text-gray-800 md:text-base"
+        >
+          Vaibhav Chopra
+        </a>
+      </Button>
+    </div>
+  );
+};
+export default LeftFooter;
